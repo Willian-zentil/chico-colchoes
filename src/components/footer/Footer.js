311d647093288d9968203aff17780e701e712008
willianZentil <willian.zentil@maeztra.com>
2024-01-07 21:53:24 -0300

    fix#imagens

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -14,11 +14,11 @@ function Footer() {
             <ContainerSmall>
                 <h3>Nos siga em nossas redes sociais</h3>
                 <div>
-                    <a href='/'><img src={facebook} alt='facebook' /></a>
+                    {/* <a href='/'><img src={facebook} alt='facebook' /></a>
                     <a href='/'><img src={Twitter} alt='Twitter' /></a>
-                    <a href='/'><img src={tiktok} alt='tiktok' /></a>
+                    <a href='/'><img src={tiktok} alt='tiktok' /></a> */}
                     <a href='/'><img src={Insta} alt='Insta' /></a>
-                    <a href='/'><img src={Youtube} alt='Youtube' /></a>
+                    {/* <a href='/'><img src={Youtube} alt='Youtube' /></a> */}
                 </div>
             </ContainerSmall>
         </footer>
--- src/components/servicos/Servicos.js
+++ src/components/servicos/Servicos.js
@@ -3,7 +3,9 @@ import styles from './Servicos.module.css'
 import ContainerSmall from '../atons/container/ContainerSmall'
 import serviceOne from '../../images/pulseira.jpeg'
 import serviceTwo from '../../images/travesseiro.jpg'
-import serviceThree from '../../images/colchao.png'
+import serviceThree from '../../images/colchao-v2.jpg'
+import serviceFour from '../../images/encosto.jpg'
+
 
 
 
@@ -17,7 +19,7 @@ function Servicos() {
                     </div>
                     <h4>Pulseiras</h4>
                     <p>
-                        auxilia na melhor curculação sanguínea, tensões musculares, retenção de líquidos, entre outros</p>
+                        Auxilia na melhor curculação sanguínea, tensões musculares, retenção de líquidos, entre outros</p>
                 </div>
                 <div className={styles.servico}>
                     <div className={styles.containerImg}>
@@ -30,15 +32,16 @@ function Servicos() {
                     <div className={styles.containerImg}>
                         <img src={serviceThree} alt='serviço' />
                     </div>
-                    <h4>colchão terapêutico</h4>
+                    <h4>Colchão terapêutico</h4>
                     <p>Nosso Colchão Magnético combina uma série de características inovadoras para oferecer não apenas um sono profundo, mas também benefícios para o seu bem-estar.</p>
                 </div>
                 <div className={styles.servico}>
                     <div className={styles.containerImg}>
-                        <img src={serviceOne} alt='serviço' />
+                        <img src={serviceFour} alt='serviço' />
                     </div>
-                    <h4>encosto</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a justo nec tellus tempus dictum. Nam imperdiet a lacus nec tincidunt</p>
+                    <h4>Encosto</h4>
+                    <p>Alivia tensão nas costas e membros inferiores, contra cãibras, dormência nas pernas, dores musculares e coluna;
+                        Auxilia na redução do estresse.</p>
                 </div>
             </ContainerSmall>
             <h3>Um bom colchão terapêutico transforma seu sono em bem-estar.</h3>
